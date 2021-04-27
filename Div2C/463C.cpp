2e0c1bec8166457e7fe6271a6602a16f9a36e22d
kunal1244 <kunaldas1244@gmail.com>
2021-04-27 16:57:11 +0530

    27/4

--- Div2C/463C.cpp
+++ Div2C/463C.cpp
@@ -2,8 +2,31 @@
 using namespace std;
 #define ll long long int
 
+ll arr[2010][2010], dplus[4010], dminus[4010];
+
+ll cellval(ll i, ll j){
+    return dplus[i + j] + dminus[2000 + i - j] - arr[i][j];
+}
+
 void solve(){
-    
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++){
+        for (ll j = 1; j <= n; j++){
+            cin >> arr[i][j];
+            dplus[i + j] += arr[i][j];
+            dminus[2000 + i - j] += arr[i][j];
+        }
+    }
+    pair<int, int> values[2];
+    for (ll i = 1; i <= n; i++){
+        for (ll j = 1; j <= n; j++){
+            ll rem = (i + j) % 2;
+            if (cellval(i, j) >= cellval(values[rem].first, values[rem].second)) values[rem] = {i, j};
+        }
+    }
+    cout << cellval(values[0].first, values[0].second) + cellval(values[1].first, values[1].second) << endl;
+    cout << values[0].first << " " << values[0].second << " " << values[1].first << " " << values[1].second << endl;
 }
 
 int main(){
